311d7f7a3347417ae6d9d595a281e8a7e59e6f0f
Jesus Santana <jesus95santana@yahoo.com>
2024-01-18 12:21:08 -0500

    Installed fresh wordpress repository

---
+++ wp-content/plugins/wp-super-cache/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/wp-super-cache',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-device-detection' => array(
+            'pretty_version' => 'v1.4.27',
+            'version' => '1.4.27.0',
+            'reference' => '5fa2add941f3a7933b34bc3f57b4649e887c3976',
+            'type' => 'jetpack-library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/wp-super-cache' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
