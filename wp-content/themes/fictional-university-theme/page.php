f4a05f16dc8dc975751fb72ef596db42eb306a2b
Jesus Santana <jesus95santana@yahoo.com>
2024-01-21 07:58:56 -0500

    Section 5 | Pages #15 (complete)

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,10 +1,44 @@
-<?= get_header() ?>
+<?php echo get_header(); ?>
 
-    <h1>I am a single Page</h1>
-<?php while ( have_posts() ) : the_post(); ?>
-    <h2><?= the_title(); ?></h2>
-    <p><?= the_content() ?></p>
-    <hr>
+
+<?php
+while ( have_posts() ) :
+	the_post();
+	?>
+	<div class="page-banner">
+		<div class="page-banner__bg-image"
+			style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ); ?>)"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title"><?php the_title(); ?></h1>
+			<div class="page-banner__intro">
+				<p>Dont forget to place me later.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container container--narrow page-section">
+		<div class="metabox metabox--position-up metabox--with-home-link">
+			<p>
+				<a class="metabox__blog-home-link" href="#"><i class="fa fa-home"
+																aria-hidden="true"></i> Back to About
+					Us</a> <span class="metabox__main">Our History</span>
+			</p>
+		</div>
+		<!--
+				<div class="page-links">
+					<h2 class="page-links__title"><a href="#">About Us</a></h2>
+					<ul class="min-list">
+						<li class="current_page_item"><a href="#">Our History</a></li>
+						<li><a href="#">Our Goals</a></li>
+					</ul>
+				</div>
+		-->
+		<div class="generic-content">
+			<?php the_content(); ?>
+		</div>
+	</div>
+
+
 <?php endwhile; ?>
 
 
--- wp-content/themes/fictional-university-theme/single.php
+++ wp-content/themes/fictional-university-theme/single.php
@@ -1,12 +1,19 @@
-<?= get_header() ?>
+<?php echo get_header(); ?>
 
-    <h1>I am a single Blog Page</h1>
-<?php while ( have_posts() ) : the_post(); ?>
-    <h2><?= the_title() ?></h2>
-    <p><?= the_content() ?></p>
-    <hr>
+
+	<h1>I am a single Blog Page</h1>
+<?php
+while ( have_posts() ) :
+	the_post();
+	?>
+	<h2><?php the_title(); ?></h2>
+	<p><?php the_content(); ?></p>
+	<hr>
+
+
 <?php endwhile; ?>
 
 
 <?php
 get_footer();
+
