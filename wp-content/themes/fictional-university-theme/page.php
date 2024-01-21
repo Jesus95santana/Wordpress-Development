d23db2de7ab36c24dce27180ce5b2e68a9035e28
Jesus Santana <jesus95santana@yahoo.com>
2024-01-21 10:39:59 -0500

    Section 5 | Pages #16,#17 (complete)

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -17,13 +17,19 @@ while ( have_posts() ) :
 	</div>
 
 	<div class="container container--narrow page-section">
-		<div class="metabox metabox--position-up metabox--with-home-link">
-			<p>
-				<a class="metabox__blog-home-link" href="#"><i class="fa fa-home"
-																aria-hidden="true"></i> Back to About
-					Us</a> <span class="metabox__main">Our History</span>
-			</p>
-		</div>
+		<?php if ( wp_get_post_parent_id() ) : ?>
+			<div class="metabox metabox--position-up metabox--with-home-link">
+				<p>
+					<a class="metabox__blog-home-link"
+						href="<?php echo get_permalink( wp_get_post_parent_id() ); ?>"><i
+								class="fa fa-home"
+								aria-hidden="true"></i> Back to
+						<?php echo get_the_title( wp_get_post_parent_id() ); ?></a> <span
+							class="metabox__main"><?php the_title(); ?></span>
+				</p>
+			</div>
+		<?php endif; ?>
+
 		<!--
 				<div class="page-links">
 					<h2 class="page-links__title"><a href="#">About Us</a></h2>
