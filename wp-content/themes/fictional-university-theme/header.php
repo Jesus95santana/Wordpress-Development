f2c2bb2e6bc7feaa05c211c82620553b50f23bc6
Jesus Santana <jesus95santana@yahoo.com>
2024-01-22 09:04:35 -0500

    Section 6 | Building the Blog Section #22,#23,#24,#25,#26 (complete)

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -18,21 +18,31 @@
 		<div class="site-header__menu group">
 			<nav class="main-navigation">
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'headerMenuLocation',
-					)
-				)
+				//              wp_nav_menu(
+				//                  array(
+				//                      'theme_location' => 'headerMenuLocation',
+				//                  )
+				//              )
 				?>
-				<!--
 				<ul>
-					<li><a href="<?php echo site_url( '/about-us' ); ?>">About Us</a></li>
+					<li
+						<?php
+						if ( is_page( 'about-us' ) || wp_get_post_parent_id( 0 ) === 12 ) {
+							echo 'class="current-menu-item"';
+						}
+						?>
+					><a href="<?php echo site_url( '/about-us' ); ?>">About Us</a></li>
 					<li><a href="#">Programs</a></li>
 					<li><a href="#">Events</a></li>
 					<li><a href="#">Campuses</a></li>
-					<li><a href="#">Blog</a></li>
+					<li
+						<?php
+						if ( get_post_type() === 'post' ) {
+							echo 'class="current-menu-item"';
+						}
+						?>
+					><a href="<?php echo site_url( '/blog' ); ?>">Blog</a></li>
 				</ul>
-				-->
 			</nav>
 			<div class="site-header__util">
 				<a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
