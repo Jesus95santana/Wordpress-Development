8261ea9bf0dcd146e5ebcb8549431f4c441e7466
Jesus Santana <jesus95santana@yahoo.com>
2024-01-30 07:55:58 -0500

    Section 25 | Plugin: React #139,#140,#141 (complete)

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -8,13 +8,38 @@ import {
 } from '@wordpress/components';
 import './index.scss';
 
+( function () {
+	let locked = false;
+
+	wp.data.subscribe( function () {
+		const results = wp.data
+			.select( 'core/block-editor' )
+			.getBlocks()
+			.filter( function ( block ) {
+				return (
+					block.name === 'ourplugin/are-you-paying-attention' &&
+					block.attributes.correctAnswer === undefined
+				);
+			} );
+		if ( results.length && locked === false ) {
+			locked = true;
+			wp.data.dispatch( 'core/editor' ).lockPostSaving( 'noanswer' );
+		}
+		if ( ! results.length && locked ) {
+			locked = false;
+			wp.data.dispatch( 'core/editor' ).unlockPostSaving( 'noanswer' );
+		}
+	} );
+} )();
+
 wp.blocks.registerBlockType( 'ourplugin/are-you-paying-attention', {
 	title: 'Are You Paying Attention?',
 	icon: 'smiley',
 	category: 'common',
 	attributes: {
-		skyColor: { type: 'string' },
-		grassColor: { type: 'string' },
+		question: { type: 'string' },
+		answers: { type: 'array', default: [ '' ] },
+		correctAnswer: { type: 'number', default: undefined },
 	},
 	edit: EditComponent,
 	save() {
@@ -23,39 +48,96 @@ wp.blocks.registerBlockType( 'ourplugin/are-you-paying-attention', {
 } );
 
 function EditComponent( props ) {
-	function updateSkyColor( { target } ) {
-		props.setAttributes( { skyColor: target.value } );
+	function updateQuestion( value ) {
+		props.setAttributes( { question: value } );
 	}
 
-	function updateGrassColor( { target } ) {
-		props.setAttributes( { grassColor: target.value } );
+	function deleteAnswer( indexDelete ) {
+		const newAnswers = props.attributes.answers.filter(
+			function ( x, index ) {
+				return index !== indexDelete;
+			}
+		);
+		props.setAttributes( { answers: newAnswers } );
+
+		if ( indexDelete === props.attributes.correctAnswer ) {
+			props.setAttributes( { correctAnswer: undefined } );
+		}
+	}
+
+	function markCorrect( index ) {
+		props.setAttributes( { correctAnswer: index } );
 	}
 
 	return (
 		<div className="paying-attention-edit-block">
-			<TextControl label="Question:" style={ { fontSize: '20px' } } />
+			<TextControl
+				label="Question:"
+				value={ props.attributes.question }
+				onChange={ updateQuestion }
+				style={ { fontSize: '20px' } }
+			/>
 			<p style={ { fontSize: '13px', margin: '20px 0 8px 0' } }>
 				Answers:
 			</p>
-			<Flex>
-				<FlexBlock>
-					<TextControl />
-				</FlexBlock>
-				<FlexItem>
-					<Button>
-						<Icon
-							className={ 'mark-as-correct' }
-							icon="star-empty"
-						/>
-					</Button>
-				</FlexItem>
-				<FlexItem>
-					<Button isLink className={ 'attention-delete' }>
-						Delete
-					</Button>
-				</FlexItem>
-			</Flex>
-			<Button isPrimary>Add another answer</Button>
+			{ props.attributes.answers.map( ( answer, index ) => {
+				return (
+					<Flex>
+						<FlexBlock>
+							<TextControl
+								autoFocus={ answer === undefined }
+								value={ answer }
+								onChange={ ( newValue ) => {
+									const newAnswers =
+										props.attributes.answers.concat( [] );
+									newAnswers[ index ] = newValue;
+									props.setAttributes( {
+										answers: newAnswers,
+									} );
+								} }
+							/>
+						</FlexBlock>
+						<FlexItem>
+							<Button>
+								<Icon
+									onClick={ () => {
+										markCorrect( index );
+									} }
+									className={ 'mark-as-correct' }
+									icon={
+										props.attributes.correctAnswer === index
+											? 'star-filled'
+											: 'star-empty'
+									}
+								/>
+							</Button>
+						</FlexItem>
+						<FlexItem>
+							<Button
+								isLink
+								className={ 'attention-delete' }
+								onClick={ () => {
+									deleteAnswer( index );
+								} }
+							>
+								Delete
+							</Button>
+						</FlexItem>
+					</Flex>
+				);
+			} ) }
+			<Button
+				isPrimary
+				onClick={ () => {
+					props.setAttributes( {
+						answers: props.attributes.answers.concat( [
+							undefined,
+						] ),
+					} );
+				} }
+			>
+				Add another answer
+			</Button>
 		</div>
 	);
 }
