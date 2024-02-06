f524f5a565e3ed817d116e0af714c083f6bf2ae2
Jesus Santana <jesus95santana@yahoo.com>
2024-02-06 08:02:49 -0500

    Section 8 | Programs Post Type #38 (complete)

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -9,82 +9,83 @@
 			<h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
 			<h3 class="headline headline--small">Why don&rsquo;t you check out the
 				<strong>major</strong> you&rsquo;re interested in?</h3>
-			<a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+			<a href="<?php echo get_post_type_archive_link( 'program' ); ?>"
+				class="btn btn--large btn--blue">Find Your Major</a>
 		</div>
 	</div>
 
-    <div class="full-width-split group">
-        <div class="full-width-split__one">
-            <div class="full-width-split__inner">
-                <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-	            <?php
-	            $today          = date( 'Ymd' );
-	            $homepageEvents = new WP_Query(
-		            array(
-			            'posts_per_page' => 2,
-			            'post_type'      => 'event',
-			            'meta_key'       => 'event_date',
-			            'orderby'        => 'meta_value_num',
-			            'order'          => 'ASC',
-			            'meta_query'     => array(
-				            array(
-					            'key'     => 'event_date',
-					            'compare' => '>=',
-					            'value'   => $today,
-					            'type'    => 'numeric',
-				            ),
-			            ),
-		            )
-	            );
+	<div class="full-width-split group">
+		<div class="full-width-split__one">
+			<div class="full-width-split__inner">
+				<h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+				<?php
+				$today          = date( 'Ymd' );
+				$homepageEvents = new WP_Query(
+					array(
+						'posts_per_page' => 2,
+						'post_type'      => 'event',
+						'meta_key'       => 'event_date',
+						'orderby'        => 'meta_value_num',
+						'order'          => 'ASC',
+						'meta_query'     => array(
+							array(
+								'key'     => 'event_date',
+								'compare' => '>=',
+								'value'   => $today,
+								'type'    => 'numeric',
+							),
+						),
+					)
+				);
 
-	            while ( $homepageEvents->have_posts() ) :
-		            $homepageEvents->the_post();
+				while ( $homepageEvents->have_posts() ) :
+					$homepageEvents->the_post();
 
-		            ?>
-                    <div class="event-summary">
-                        <a class="event-summary__date t-center" href="#">
+					?>
+					<div class="event-summary">
+						<a class="event-summary__date t-center" href="#">
 							<span class="event-summary__month">
 							<?php
 							$eventDate = new DateTime( get_field( 'event_date' ) );
 							echo $eventDate->format( 'M' );
 							?>
 								</span>
-                            <span class="event-summary__day">
+							<span class="event-summary__day">
 							<?php
 							echo $eventDate->format( 'd' );
 							?>
 								</span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a
-                                        href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
-                            </h5>
-                            <p>
-					            <?php
-					            if ( has_excerpt() ) {
-						            echo get_the_excerpt();
-					            } else {
-						            echo wp_trim_words( get_the_content(), 18 );
-					            }
-					            ?>
-                                <a
-                                        href="<?php echo the_permalink(); ?>"
-                                        class="nu gray">Learn
-                                    more</a></p>
-                        </div>
-                    </div>
+						</a>
+						<div class="event-summary__content">
+							<h5 class="event-summary__title headline headline--tiny"><a
+										href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a>
+							</h5>
+							<p>
+								<?php
+								if ( has_excerpt() ) {
+									echo get_the_excerpt();
+								} else {
+									echo wp_trim_words( get_the_content(), 18 );
+								}
+								?>
+								<a
+										href="<?php echo the_permalink(); ?>"
+										class="nu gray">Learn
+									more</a></p>
+						</div>
+					</div>
 
-	            <?php endwhile ?>
+				<?php endwhile ?>
 
-                <p class="t-center no-margin"><a
-                            href="<?php echo get_post_type_archive_link( 'event' ); ?>"
-                            class="btn btn--blue">View All Events</a>
-                </p>
-            </div>
-        </div>
-        <div class="full-width-split__two">
-            <div class="full-width-split__inner">
-                <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+				<p class="t-center no-margin"><a
+							href="<?php echo get_post_type_archive_link( 'event' ); ?>"
+							class="btn btn--blue">View All Events</a>
+				</p>
+			</div>
+		</div>
+		<div class="full-width-split__two">
+			<div class="full-width-split__inner">
+				<h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
 				<?php
 				$home_page_posts = new WP_Query(
 					array(
@@ -94,17 +95,17 @@
 				while ( $home_page_posts->have_posts() ) :
 					$home_page_posts->the_post();
 					?>
-                    <div class="event-summary">
-                        <a class="event-summary__date event-summary__date--beige t-center"
-                           href="<?php the_permalink(); ?>">
-                            <span class="event-summary__month"><?php the_time( 'M' ); ?></span>
-                            <span class="event-summary__day"><?php the_time( 'd' ); ?></span>
-                        </a>
-                        <div class="event-summary__content">
-                            <h5 class="event-summary__title headline headline--tiny"><a
-                                        href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            </h5>
-                            <p>
+					<div class="event-summary">
+						<a class="event-summary__date event-summary__date--beige t-center"
+							href="<?php the_permalink(); ?>">
+							<span class="event-summary__month"><?php the_time( 'M' ); ?></span>
+							<span class="event-summary__day"><?php the_time( 'd' ); ?></span>
+						</a>
+						<div class="event-summary__content">
+							<h5 class="event-summary__title headline headline--tiny"><a
+										href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</h5>
+							<p>
 								<?php
 								if ( has_excerpt() ) {
 									echo get_the_excerpt();
@@ -112,63 +113,63 @@
 									echo wp_trim_words( get_the_content(), 18 );
 								}
 								?>
-                                <a
-                                        href="<?php the_permalink(); ?>"
-                                        class="nu gray">Read
-                                    more</a></p>
-                        </div>
-                    </div>
+								<a
+										href="<?php the_permalink(); ?>"
+										class="nu gray">Read
+									more</a></p>
+						</div>
+					</div>
 					<?php wp_reset_postdata(); endwhile ?>
 
 
-                <p class="t-center no-margin"><a href="<?php echo site_url( '/blog' ); ?>"
-                                                 class="btn btn--yellow">View All Blog
-                        Posts</a></p>
-            </div>
-        </div>
-    </div>
+				<p class="t-center no-margin"><a href="<?php echo site_url( '/blog' ); ?>"
+												class="btn btn--yellow">View All Blog
+						Posts</a></p>
+			</div>
+		</div>
+	</div>
 
-    <div class="hero-slider">
-        <div data-glide-el="track" class="glide__track">
-            <div class="glide__slides">
-                <div class="hero-slider__slide"
-                     style="background-image: url(<?php echo get_theme_file_uri( '/images/bus.jpg' ); ?>)">
-                    <div class="hero-slider__interior container">
-                        <div class="hero-slider__overlay">
-                            <h2 class="headline headline--medium t-center">Free Transportation</h2>
-                            <p class="t-center">All students have free unlimited bus fare.</p>
-                            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
-                                    more</a></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="hero-slider__slide"
-                     style="background-image: url(<?php echo get_theme_file_uri( '/images/apples.jpg' ); ?>)">
-                    <div class="hero-slider__interior container">
-                        <div class="hero-slider__overlay">
-                            <h2 class="headline headline--medium t-center">An Apple a Day</h2>
-                            <p class="t-center">Our dentistry program recommends eating apples.</p>
-                            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
-                                    more</a></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="hero-slider__slide"
-                     style="background-image: url(<?php echo get_theme_file_uri( '/images/bread.jpg' ); ?>)">
-                    <div class="hero-slider__interior container">
-                        <div class="hero-slider__overlay">
-                            <h2 class="headline headline--medium t-center">Free Food</h2>
-                            <p class="t-center">Fictional University offers lunch plans for those in
-                                need.</p>
-                            <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
-                                    more</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
-        </div>
-    </div>
+	<div class="hero-slider">
+		<div data-glide-el="track" class="glide__track">
+			<div class="glide__slides">
+				<div class="hero-slider__slide"
+					style="background-image: url(<?php echo get_theme_file_uri( '/images/bus.jpg' ); ?>)">
+					<div class="hero-slider__interior container">
+						<div class="hero-slider__overlay">
+							<h2 class="headline headline--medium t-center">Free Transportation</h2>
+							<p class="t-center">All students have free unlimited bus fare.</p>
+							<p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
+									more</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="hero-slider__slide"
+					style="background-image: url(<?php echo get_theme_file_uri( '/images/apples.jpg' ); ?>)">
+					<div class="hero-slider__interior container">
+						<div class="hero-slider__overlay">
+							<h2 class="headline headline--medium t-center">An Apple a Day</h2>
+							<p class="t-center">Our dentistry program recommends eating apples.</p>
+							<p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
+									more</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="hero-slider__slide"
+					style="background-image: url(<?php echo get_theme_file_uri( '/images/bread.jpg' ); ?>)">
+					<div class="hero-slider__interior container">
+						<div class="hero-slider__overlay">
+							<h2 class="headline headline--medium t-center">Free Food</h2>
+							<p class="t-center">Fictional University offers lunch plans for those in
+								need.</p>
+							<p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn
+									more</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+		</div>
+	</div>
 
 <?php
 get_footer();
