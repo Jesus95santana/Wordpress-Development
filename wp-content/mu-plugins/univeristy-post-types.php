81135a908d4d3f3fcd98eaacb76f764264c1b30b
Jesus Santana <jesus95santana@yahoo.com>
2024-02-06 07:15:37 -0500

    Section 8 | Programs Post Type #36 (complete)

--- wp-content/mu-plugins/univeristy-post-types.php
+++ wp-content/mu-plugins/univeristy-post-types.php
@@ -1,5 +1,6 @@
 <?php
 function university_post_types() {
+	// Event Post Type
 	register_post_type(
 		'event',
 		array(
@@ -16,6 +17,26 @@ function university_post_types() {
 				'singular_name' => 'Event',
 			),
 			'menu_icon'    => 'dashicons-calendar',
+		)
+	);
+
+	// Program Post Type
+	register_post_type(
+		'program',
+		array(
+			'supports'     => array( 'title', 'editor' ),
+			'rewrite'      => array( 'slug' => 'programs' ),
+			'has_archive'  => true,
+			'public'       => true,
+			'show_in_rest' => true,
+			'labels'       => array(
+				'name'          => 'Programs',
+				'add_new_item'  => 'Add New Program',
+				'edit_item'     => 'Edit Program',
+				'all_items'     => 'All Programs',
+				'singular_name' => 'Program',
+			),
+			'menu_icon'    => 'dashicons-awards',
 		)
 	);
 }
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -31,6 +31,11 @@ function university_features() {
 }
 
 function university_adjust_queries( $query ) {
+	if ( ! is_admin() && is_post_type_archive( 'program' ) && is_main_query() ) {
+		$query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
+		$query->set( 'posts_per_page', - 1 );
+	}
 	if ( ! is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query() ) {
 		$today = date( 'Ymd' );
 		$query->set( 'meta_key', 'event_date', );
