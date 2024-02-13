9e1ed5774ed2b19d5d9210596b1b724e0b930367
Jesus Santana <jesus95santana@yahoo.com>
2024-02-13 07:20:38 -0500

    Section 10 | Cleaner Code #46, #47, #48 (complete)

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -2,14 +2,15 @@
 
 function pageBanner( $args = null ) {
 	// php logic will live here
-	if ( ! $args['title'] ) {
+
+	if ( ! isset( $args['title'] ) ) {
 		$args['title'] = get_the_title();
 	}
-	if ( ! $args['subtitle'] ) {
+	if ( ! isset( $args['subtitle'] ) ) {
 		$args['subtitle'] = get_field( 'page_banner_subtitle' );
 	}
-	if ( ! $args['photo'] ) {
-		if ( get_field( 'page_banner_background_image' ) ) {
+	if ( ! isset( $args['photo'] ) ) {
+		if ( get_field( 'page_banner_background_image' ) && ! is_archive() && ! is_home() ) {
 			$args['photo'] = get_field( 'page_banner_background_image' )['sizes']['pageBanner'];
 		} else {
 			$args['photo'] = get_theme_file_uri( '/images/ocean.jpg' );
