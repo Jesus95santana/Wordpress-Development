7591cad8b471fe8251354e1c821c41d3503f1257
Jesus Santana <jesus95santana@yahoo.com>
2024-02-20 07:36:52 -0500

    Section 14 | REST API (AJAX) #70  (complete)

--- wp-content/themes/fictional-university-theme/build/index.js
+++ wp-content/themes/fictional-university-theme/build/index.js
@@ -153,13 +153,16 @@ class Search {
   }
   getResults() {
     jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-      this.resultsDiv.html(`
+      jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val(), pages => {
+        const combinedResults = posts.concat(pages);
+        this.resultsDiv.html(`
 				<h2 class ="search-overlay__section-title">General Information</h2>
-				${posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
-				${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-				${posts.length ? '</ul>' : ''}
+				${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search</p>'}
+				${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+				${combinedResults.length ? '</ul>' : ''}
 				`);
-      this.isSpinning = false;
+        this.isSpinning = false;
+      });
     });
   }
   keyPressDispatcher(e) {
--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -55,22 +55,30 @@ class Search {
 				'/wp-json/wp/v2/posts?search=' +
 				this.searchField.val(),
 			( posts ) => {
-				this.resultsDiv.html( `
+				$.getJSON(
+					universityData.root_url +
+						'/wp-json/wp/v2/pages?search=' +
+						this.searchField.val(),
+					( pages ) => {
+						const combinedResults = posts.concat( pages );
+						this.resultsDiv.html( `
 				<h2 class ="search-overlay__section-title">General Information</h2>
 				${
-					posts.length
+					combinedResults.length
 						? '<ul class="link-list min-list">'
 						: '<p>No general information matches that search</p>'
 				}
-				${ posts
+				${ combinedResults
 					.map(
 						( item ) =>
 							`<li><a href="${ item.link }">${ item.title.rendered }</a></li>`
 					)
 					.join( '' ) }
-				${ posts.length ? '</ul>' : '' }
+				${ combinedResults.length ? '</ul>' : '' }
 				` );
-				this.isSpinning = false;
+						this.isSpinning = false;
+					}
+				);
 			}
 		);
 	}
