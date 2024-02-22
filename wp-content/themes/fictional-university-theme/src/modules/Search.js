1931e95b9b0e963852587182d9186671ac8e74b5
Jesus Santana <jesus95santana@yahoo.com>
2024-02-22 06:59:09 -0500

    Section 15 | Customizing the REST API #72  (complete)

--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -74,7 +74,13 @@ class Search {
 				${ combinedResults
 					.map(
 						( item ) =>
-							`<li><a href="${ item.link }">${ item.title.rendered }</a></li>`
+							`<li><a href="${ item.link }">${
+								item.title.rendered
+							}</a> ${
+								item.type === 'post'
+									? `by ${ item.authorName }`
+									: ''
+							}</li>`
 					)
 					.join( '' ) }
 				${ combinedResults.length ? '</ul>' : '' }
