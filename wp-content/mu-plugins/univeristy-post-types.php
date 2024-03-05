e9ac80fc73d7a97cea2eaf552205d5c26399bcdc
Jesus Santana <jesus95santana@yahoo.com>
2024-03-05 07:25:59 -0500

    Section 18 | User Roles & Permissions #86  (complete)

--- wp-content/mu-plugins/univeristy-post-types.php
+++ wp-content/mu-plugins/univeristy-post-types.php
@@ -4,19 +4,21 @@ function university_post_types() {
 	register_post_type(
 		'event',
 		array(
-			'supports'     => array( 'title', 'editor', 'excerpt' ),
-			'rewrite'      => array( 'slug' => 'events' ),
-			'has_archive'  => true,
-			'public'       => true,
-			'show_in_rest' => true,
-			'labels'       => array(
+			'capability_type' => 'event',
+			'map_meta_cap'    => true,
+			'supports'        => array( 'title', 'editor', 'excerpt' ),
+			'rewrite'         => array( 'slug' => 'events' ),
+			'has_archive'     => true,
+			'public'          => true,
+			'show_in_rest'    => true,
+			'labels'          => array(
 				'name'          => 'Events',
 				'add_new_item'  => 'Add New Event',
 				'edit_item'     => 'Edit Event',
 				'all_items'     => 'All Events',
 				'singular_name' => 'Event',
 			),
-			'menu_icon'    => 'dashicons-calendar',
+			'menu_icon'       => 'dashicons-calendar',
 		)
 	);
 
