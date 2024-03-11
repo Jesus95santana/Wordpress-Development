6d2aefce5b550b3aa24c41b2f82b189cc9e2eb39
Jesus Santana <jesus95santana@yahoo.com>
2024-03-11 08:15:40 -0400

    Section 19 | User Generated Content #94  (complete)

--- wp-content/themes/fictional-university-theme/build/index.js
+++ wp-content/themes/fictional-university-theme/build/index.js
@@ -2174,7 +2174,8 @@ class MyNotes {
   }
   events() {
     jquery__WEBPACK_IMPORTED_MODULE_0___default()('.delete-note').on('click', this.deleteNote);
-    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.edit-note').on('click', this.editnote);
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.edit-note').on('click', this.editNote.bind(this));
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('.update-note').on('click', this.updateNote.bind(this));
   }
 
   // Methods
@@ -2197,10 +2198,51 @@ class MyNotes {
       }
     });
   }
-  editnote(e) {
+  updateNote(e) {
     const thisNote = jquery__WEBPACK_IMPORTED_MODULE_0___default()(e.target).parents('li');
+    const ourUpdatedPost = {
+      title: thisNote.find('.note-title-field').val(),
+      content: thisNote.find('.note-body-field').val()
+    };
+    jquery__WEBPACK_IMPORTED_MODULE_0___default().ajax({
+      beforeSend: xhr => {
+        xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+      },
+      url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+      type: 'POST',
+      data: ourUpdatedPost,
+      success: response => {
+        this.makeNoteReadOnly(thisNote);
+        console.log('success');
+        console.log(response);
+      },
+      error: response => {
+        console.log('error');
+        console.log(response);
+      }
+    });
+  }
+  editNote(e) {
+    const thisNote = jquery__WEBPACK_IMPORTED_MODULE_0___default()(e.target).parents('li');
+    if (thisNote.data('state') === 'editable') {
+      // Make read only
+      this.makeNoteReadOnly(thisNote);
+    } else {
+      // Make Editable
+      this.makeNoteEditable(thisNote);
+    }
+  }
+  makeNoteEditable(thisNote) {
+    thisNote.find('.edit-note').html('<i class="fa fa-times" aria-hidden="true"></i>Cancel');
     thisNote.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field');
     thisNote.find('.update-note').addClass('update-note--visible');
+    thisNote.data('state', 'editable');
+  }
+  makeNoteReadOnly(thisNote) {
+    thisNote.find('.edit-note').html('<i class="fa fa-pencil" aria-hidden="true"></i>Edit');
+    thisNote.find('.note-title-field, .note-body-field').attr('readonly', 'readonly').removeClass('note-active-field');
+    thisNote.find('.update-note').removeClass('update-note--visible');
+    thisNote.data('state', 'cancel');
   }
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyNotes);
--- wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -7,7 +7,8 @@ class MyNotes {
 
 	events() {
 		$( '.delete-note' ).on( 'click', this.deleteNote );
-		$( '.edit-note' ).on( 'click', this.editnote );
+		$( '.edit-note' ).on( 'click', this.editNote.bind( this ) );
+		$( '.update-note' ).on( 'click', this.updateNote.bind( this ) );
 	}
 
 	// Methods
@@ -35,13 +36,67 @@ class MyNotes {
 		} );
 	}
 
-	editnote( e ) {
+	updateNote( e ) {
 		const thisNote = $( e.target ).parents( 'li' );
+		const ourUpdatedPost = {
+			title: thisNote.find( '.note-title-field' ).val(),
+			content: thisNote.find( '.note-body-field' ).val(),
+		};
+		$.ajax( {
+			beforeSend: ( xhr ) => {
+				xhr.setRequestHeader( 'X-WP-Nonce', universityData.nonce );
+			},
+			url:
+				universityData.root_url +
+				'/wp-json/wp/v2/note/' +
+				thisNote.data( 'id' ),
+			type: 'POST',
+			data: ourUpdatedPost,
+			success: ( response ) => {
+				this.makeNoteReadOnly( thisNote );
+				console.log( 'success' );
+				console.log( response );
+			},
+			error: ( response ) => {
+				console.log( 'error' );
+				console.log( response );
+			},
+		} );
+	}
+
+	editNote( e ) {
+		const thisNote = $( e.target ).parents( 'li' );
+		if ( thisNote.data( 'state' ) === 'editable' ) {
+			// Make read only
+			this.makeNoteReadOnly( thisNote );
+		} else {
+			// Make Editable
+			this.makeNoteEditable( thisNote );
+		}
+	}
+
+	makeNoteEditable( thisNote ) {
+		thisNote
+			.find( '.edit-note' )
+			.html( '<i class="fa fa-times" aria-hidden="true"></i>Cancel' );
 		thisNote
 			.find( '.note-title-field, .note-body-field' )
 			.removeAttr( 'readonly' )
 			.addClass( 'note-active-field' );
 		thisNote.find( '.update-note' ).addClass( 'update-note--visible' );
+		thisNote.data( 'state', 'editable' );
+	}
+
+	makeNoteReadOnly( thisNote ) {
+		thisNote
+			.find( '.edit-note' )
+			.html( '<i class="fa fa-pencil" aria-hidden="true"></i>Edit' );
+		thisNote
+			.find( '.note-title-field, .note-body-field' )
+			.attr( 'readonly', 'readonly' )
+			.removeClass( 'note-active-field' );
+		thisNote.find( '.update-note' ).removeClass( 'update-note--visible' );
+		thisNote.data( 'state', 'cancel' );
 	}
 }
 
