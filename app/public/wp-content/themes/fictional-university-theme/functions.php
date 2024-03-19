24dc2ea1cd8af9fb764845ed1d312ec1b14476e1
Jesus Santana <jesus95santana@yahoo.com>
2024-03-19 07:47:36 -0400

    Wordpress Development: 19 | User Generated Content #98
    - Progress: complete

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -173,7 +173,11 @@ function ourLoginTitle() {
 // Force note posts to be private
 add_filter( 'wp_insert_post_data', 'makeNotePrivate' );
 function makeNotePrivate( $data ) {
-	if ( $data['post_type'] === 'note' || $data['post_status'] != 'trash' ) {
+	if ( $data['post_type'] === 'note' ) {
+		$data['post_content'] = sanitize_textarea_field( $data['post_content'] );
+		$data['post_title']   = sanitize_text_field( $data['post_title'] );
+	}
+	if ( $data['post_type'] === 'note' && $data['post_status'] !== 'trash' ) {
 		$data['post_status'] = 'private';
 	}
 
