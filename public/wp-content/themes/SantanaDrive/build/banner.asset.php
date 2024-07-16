eda50fc24fcae066e6db3eb458428253d99f739e
Jesus Santana <jesus95santana@yahoo.com>
2024-07-16 06:54:37 -0400

    Remove ignored files from repository

--- public/wp-content/themes/SantanaDrive/build/banner.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'd46f5f10d2f0ecbc47de');
--- public/wp-content/themes/SantanaDrive/build/genericheading.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '5df9f92a4a66ca6a3c69');
